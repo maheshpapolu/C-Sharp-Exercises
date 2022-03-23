286c311147101c42fdf02d43f1dff65f66c045bf
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-24 01:51:41 +0530

    Make if and else together files

---
+++ Make if and else together/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Make_if_and_else_together
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 50;
+            int b = 30;
+            int c = 40;
+
+            if ((a + b + c > 100) && (a == b)) // the &&  means both conditions must be true to execute the statement
+            {
+                Console.WriteLine("The answer is grater than 100");
+                Console.WriteLine("And the first number is  equal to the second number."); 
+            }
+
+            else
+            {
+                Console.WriteLine("The answer is not grater than the 100");
+                Console.WriteLine("or the first number is not equal to the second number.");
+            }
+            
+        }
+    }
+}
