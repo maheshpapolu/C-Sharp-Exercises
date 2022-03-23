4441e19a29a0e8ea8f0219811896215b9a235c7f
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-24 00:14:46 +0530

    [mahesh]my first change

---
+++ Declare and use varibles/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Declare_and_use_varibles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declares a Varible , and assigns it a value
+            
+            string aFriend = "John";        
+
+            // Prints out the value or Name
+
+            Console.WriteLine(aFriend);      // Jhon
+
+            //Adding "Hello" message to aFriend using + 
+
+            Console.WriteLine("Hello " + aFriend); // Hello Jhon
+
+            // Using $ symbol to pass a message
+
+            Console.WriteLine( $"Hello {aFriend}");
+
+            // find the length of a string using "Length"
+
+            Console.WriteLine($"The name {aFriend} has {aFriend.Length} Letters");
+        }
+    }
+}
---
+++ Work with C-sharp basics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_with_C_sharp_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // write my first code on C#
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
