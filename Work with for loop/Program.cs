370b0c3c6737bb5912d580ebfbec705b15856820
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-24 02:02:51 +0530

    added Work with for loop files

---
+++ Work with for loop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_with_for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Value of i: {0}", i);
+            }
+        }
+    }
+}
