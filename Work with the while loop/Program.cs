31f349d3e733069581d7beba83b02f5c0c3fc0a1
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-24 01:56:00 +0530

    added  Work with the while loop files

---
+++ Work with the while loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_with_the_while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0; //  varible initialization
+
+            while (i <= 10) // condition for while loop
+            {
+                Console.WriteLine("i = {0}", i);
+
+                i++; // increment
+            }
+        }
+    }
+}
