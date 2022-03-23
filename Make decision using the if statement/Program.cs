53fe87bd2c1f620e4f7b35472406fe933042198b
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-24 01:43:08 +0530

    added make decision using if files

---
+++ Make decision using the if statement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Make_decision_using_the_if_statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declared varible type, varible and it value 
+            
+            int a = 50;
+            int b = 30;
+
+            if (a + b > 70) // 80 > 70 its true 'its print the if statement'
+                Console.WriteLine("The answer is grater then 70.");
+
+
+
+        }
+    }
+}
